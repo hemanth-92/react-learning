46ef5199df2205f4664f0d8c4ab92db639dcbb3e
hemanth-92 <hemanth9679@gmail.com>
2024-03-20 17:57:13 +0530

    react and jsx

--- vite-recat/src/main.jsx
+++ vite-recat/src/main.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-)
-export default App
+function MyApp() {
+	return (
+		<div>
+			<h1>learning from chai aur code</h1>
+		</div>
+	);
+}
+// const reactElement = {
+// 	type: "a",
+// 	props: {
+// 		href: "https://www.google.com",
+// 		target: "_blank",
+// 	},
+// 	children: "click to visit me",
+// };
+const anotherElement = (
+	<a href="https://google.com" target="_blank">
+		visit google
+	</a>
+);
+const anotherUser = "sarath"
+const reactElement = React.createElement(
+	'a',
+	{ href: 'https://google.com', target: '_blank' }, 'click here', anotherUser
+);
+ReactDOM.createRoot(document.getElementById("root")).render(
+	reactElement
+	//<App />
+);
