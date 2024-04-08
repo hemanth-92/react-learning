10331b037f736a2f16847d52d1a927766264d39b
hemanth-92 <131354764+hemanth-92@users.noreply.github.com>
2024-04-08 22:40:37 +0530

    react router

---
+++ reactrouter/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
+import Header from './components/Header/Header'
+function App() {
+  return (
+    <>
+      <Footer />
+      <Header />
+      <Home />
+    </>
+  )
+}
+
+export default App
