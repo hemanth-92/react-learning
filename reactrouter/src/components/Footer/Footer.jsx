511853fe2cbebaadb05882e7d2423f839fb0083e
hemanth-92 <131354764+hemanth-92@users.noreply.github.com>
2024-04-09 22:42:01 +0530

    Done

--- reactrouter/src/components/Footer/Footer.jsx
+++ reactrouter/src/components/Footer/Footer.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
 export default function Footer() {
   return (
     <footer className="bg-white border-y">
@@ -18,7 +17,9 @@ export default function Footer() {
           </div>
           <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
             <div>
-              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Resources</h2>
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">
+                Resources
+              </h2>
               <ul className="text-gray-500 font-medium">
                 <li className="mb-4">
                   <Link to="/" className="hover:underline">
@@ -33,11 +34,13 @@ export default function Footer() {
               </ul>
             </div>
             <div>
-              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Follow us</h2>
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">
+                Follow us
+              </h2>
               <ul className="text-gray-500 font-medium">
                 <li className="mb-4">
                   <a
-                    href="https://github.com/hiteshchoudhary"
+                    href="https://github.com/hemanth-92"
                     className="hover:underline"
                     target="_blank"
                     rel="noreferrer"
@@ -53,7 +56,9 @@ export default function Footer() {
               </ul>
             </div>
             <div>
-              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Legal</h2>
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">
+                Legal
+              </h2>
               <ul className="text-gray-500 font-medium">
                 <li className="mb-4">
                   <Link to="#" className="hover:underline">
@@ -74,7 +79,7 @@ export default function Footer() {
           <span className="text-sm text-gray-500 sm:text-center">
             © 2023
             <a href="https://hiteshchoudhary.com/" className="hover:underline">
-              hiteshchoudhary
+              Hemanth
             </a>
             . All Rights Reserved.
           </span>
@@ -161,4 +166,3 @@ export default function Footer() {
     </footer>
   );
 }
-
