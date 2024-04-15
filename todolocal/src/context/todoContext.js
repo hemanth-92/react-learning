92c52c786961724bab6c0bf24265b84ff3fa81b7
hemanth-92 <131354764+hemanth-92@users.noreply.github.com>
2024-04-15 20:17:38 +0530

    todolocal

--- todolocal/src/context/todoContext.js
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from "react";
-
-export const TodoContext = createContext({})
-
-export const useTodo = () => {
-  return useContext(useTodo)
-}
-
-export const TodoProvider = TodoContext.Provider
